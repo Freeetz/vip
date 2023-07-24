c34b41118e220c2139ca76332c1d11d3e7d8b7fd
Freetz <110610754+Freeetz@users.noreply.github.com>
2023-07-24 02:03:37 +0200

    Ajout du SQL 😅

---
+++ sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `vips` (
+  `identifier` varchar(50) DEFAULT NULL,
+  `vip` int(11) DEFAULT NULL,
+  `expiration` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
